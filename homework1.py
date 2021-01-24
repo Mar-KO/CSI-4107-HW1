bc259345749cf547f0e0127f2b16a279a2d44efe
Mar-KO <43188422+Mar-KO@users.noreply.github.com>
2021-01-24 10:29:18 -0500

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,4 +1,11 @@
 import spacy
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
 # The core spacy object that will be used for tokenization, lemmatization, POS Tagging, ...
 # Note that this is specifically for the English language and requires the English package to be installed
 # via pip to work as intended.
