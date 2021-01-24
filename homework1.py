57986e004e8ca6083d9a76797d07708eb87b9e0b
Mar-KO <43188422+Mar-KO@users.noreply.github.com>
2021-01-24 10:08:08 -0500

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,3 +1,9 @@
 # Before starting we will import every module that we will be using
 import spacy
 import pandas as pd
+
+# The core spacy object that will be used for tokenization, lemmatization, POS Tagging, ...
+# Note that this is specifically for the English language and requires the English package to be installed
+# via pip to work as intended.
+
+sp = spacy.load("en_core_web_sm")
