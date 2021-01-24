66046b4970f0f188df16d700f0e97bb9db553aff
Mar-KO <43188422+Mar-KO@users.noreply.github.com>
2021-01-24 10:13:46 -0500

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -2,12 +2,27 @@
 import spacy
 import pandas as pd
 
-# The core spacy object that will be used for tokenization, lemmatization, POS Tagging, ...
-# Note that this is specifically for the English language and requires the English package to be installed
-# via pip to work as intended.
+name: your-workflow-name
+on: [push]
+jobs:
+  run:
+    runs-on: [ubuntu-latest]
+    container: docker://dvcorg/cml-py3:latest
+    steps:
+      - uses: actions/checkout@v2
+      - name: cml_run
+        env:
+          repo_token: ${{ secrets.GITHUB_TOKEN }}
+        run: |
+          # The core spacy object that will be used for tokenization, lemmatization, POS Tagging, ...
+          # Note that this is specifically for the English language and requires the English package to be installed
+          # via pip to work as intended.
 
-sp = spacy.load("en_core_web_sm")
+          sp = spacy.load("en_core_web_sm")
 
-#read the tweets
-df = pd.read_csv("Twitter.txt")
-df.head(10)
+          #read the tweets
+          df = pd.read_csv("Twitter.txt")
+          df.head(10)
+
+          
+
