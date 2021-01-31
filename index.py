45daf16f6455f7a493d6b7427e28f9f6da11cfb2
Daryl Dahoo <dahoo097@gmail.com>
2021-01-31 15:11:07 -0500

    modified index, added tokenizer

--- index.py
+++ index.py
@@ -1,20 +1,31 @@
 import spacy as sp
+import pandas
 
 class InvertedIndex(object):
     
 
     def __init__(self):
         self._hashtable = dict()
+        self._documents = dict()
 
-    def addDocument(self, token, document):
+    # document will either be a Series or a DataFrame, 
+    # using the tweettime as the key, and the Doc as the value
+    def addDocument(self, document):
+        self._documents[document['querytweettime']] = document['doc']
+    
+    # token is a string, document is a DataFrame
+    def addToken(self, token, document):
         if (self._hashtable.get(token) == None):
             self._hashtable[token] = list()
-        self._hashtable[token].append(document)
+            self._hashtable[token].append((document['querytweettime'], 0))
+        else:
+            self._hashtable[token]
+        
     
     def tokenCount(self, token):
         if (self._hashtable.get(token) == None):
-            0
+            return 0
         else:
-            len(self._hashtable[token])
+            return len(self._hashtable[token])
 
     
