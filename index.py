b789b936ce75ed6078e4ab3c13885f94e092e806
Daryl Dahoo <dahoo097@gmail.com>
2021-01-31 16:52:44 -0500

    modify index and notebook

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 import spacy as sp
 import pandas
 
+
+
+
+
 class InvertedIndex(object):
     
 
@@ -13,19 +17,28 @@ class InvertedIndex(object):
     def addDocument(self, document):
         self._documents[document['querytweettime']] = document['doc']
     
-    # token is a string, document is a DataFrame
+    # token is a string, document is the document ID
     def addToken(self, token, document):
         if (self._hashtable.get(token) == None):
-            self._hashtable[token] = list()
-            self._hashtable[token].append((document['querytweettime'], 0))
+            self._hashtable[token] = dict()
+            self._hashtable[token].update({document: 0})
+        else if (self._hashtable[token] != None and self._hashtable[token][document] != None):
+            self._hashtable[token][document] = 0
         else:
-            self._hashtable[token]
+            self._hashtable[token][document]++
         
     
-    def tokenCount(self, token):
+    def tokenDf(self, token):
         if (self._hashtable.get(token) == None):
             return 0
         else:
             return len(self._hashtable[token])
-
     
+
+# docs param is a DataFrame, inverted_index is a ref to InvertedIndex
+def indexDocs(docs, inverted_index):
+    doc = docs['doc']
+    docId = docs['querytweettime']
+    for token in doc:
+        inverted_index.addDocument(docs)
+        inverted_index.addToken(token.text, docId)
